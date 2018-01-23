df8d66f4656a9086ef6c8dc7546dfa0a50c80277
WhiteSnow <rgb92911.0.3@gmail.com>
2018-01-24 01:10:13 +0800

    top1000

---
+++ config.js
@@ -0,0 +1,12 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'vSfrKvDkdx0I0yNhFwWjZuWnu',  
+    consumer_secret: 'og7rAvxFISEMlYvik167b8Hwfw1zY9eGjTSJQpXzynHQwoyATI',
+    access_token: '955558366469799936-rKccFRqKFIAsFqwCvBCDiJAdXmMZzAv',  
+    access_token_secret: 'EVqgM74ZM2yOmh8PdTr7qgi7174bQWaPLeQo3iMb3c15r'
+  }
